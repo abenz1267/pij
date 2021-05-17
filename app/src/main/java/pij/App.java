ac29b5b63d4fdee8c8223ed51a015fcfba5b1655
timm_a <timmandre23@gmail.com>
2021-05-17 12:17:34 +0200

    bla

--- app/src/main/java/pij/App.java
+++ app/src/main/java/pij/App.java
@@ -1,22 +1,35 @@
 package pij;
 
+import javafx.
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
 
 public class App extends Application {
   @Override
-  public void start(Stage stage) {
+  public void start(Stage stage) throws IOException {
+
     String javaVersion = System.getProperty("java.version");
     String javafxVersion = System.getProperty("javafx.version");
-    Label l =
-        new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-    Scene scene = new Scene(new StackPane(l), 640, 480);
 
-    stage.setScene(scene);
-    stage.show();
+//    Label l =
+//        new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+//    Scene scene = new Scene(new StackPane(l), 640, 480);
+//
+//    stage.setScene(scene);
+//    stage.show();
+
+    ResourceBundle config_bundle = ResourceBundle.getBundle("config");
+    String UI = config_bundle.getString("mainview");
+
+    //FXMLLoader loader = new FXMLLoader(getClass().getResource(UI));
+
+    //Scene scene = new Scene(loader.load());
+
+    String title = config_bundle.getString("title");
   }
 
   public static void main(String[] args) {
