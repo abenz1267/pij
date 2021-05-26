d9b8af962040adda0851545c6635d5b385a45635
Andrej Benz <hello@benz.dev>
2021-05-26 15:11:30 +0200

    started working on import

--- app/src/main/java/entities/media/MediaService.java
+++ app/src/main/java/entities/media/MediaService.java
@@ -2,11 +2,13 @@ package entities.media;
 
 import com.google.inject.ImplementedBy;
 import com.j256.ormlite.dao.Dao;
-import java.sql.SQLException;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 @ImplementedBy(MediaServiceImpl.class)
 public interface MediaService {
   Dao<Media, Integer> dao();
 
-  void create(Media media) throws SQLException;
+  void importMedia(List<File> files) throws IOException;
 }
--- app/src/main/java/views/SceneService.java
+++ app/src/main/java/views/SceneService.java
@@ -3,10 +3,13 @@ package views;
 import com.google.inject.ImplementedBy;
 import java.io.IOException;
 import javafx.scene.Scene;
+import javafx.stage.Window;
 
 @ImplementedBy(SceneServiceImpl.class)
 public interface SceneService {
   Scene load(View view) throws IOException;
 
   void change(View view) throws IOException;
+
+  Window getWindow();
 }
