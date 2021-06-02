bb4d4ecef9c79fa717ff4238bc7bbdbdd6341443
Christian <paulsenchristian@gmx.de>
2021-06-02 13:58:34 +0200

    image display working
    single row, needs dynamic rows

--- app/src/main/java/views/MediaViewController.java
+++ app/src/main/java/views/MediaViewController.java
@@ -1,48 +1,88 @@
 package views;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
-import java.sql.SQLDataException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+
+import com.google.inject.Inject;
 
 import entities.media.Media;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.HBox;
 
 public class MediaViewController extends AbstractController implements Initializable {
 
-    @FXML ImageView imageView;
     @FXML ScrollPane scrollPane;
+    @Inject private SceneService sceneService;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+
         eventService.register(this);
     }
 
     @FXML
-    private void initializeMediaView() {
+    public ScrollPane initializeMediaView() {
         List<Media> media;
+        HBox hb = new HBox();
+        scrollPane = new ScrollPane();
+        scrollPane.setHbarPolicy(ScrollBarPolicy.NEVER);
 
         try {
-            media = mediaService.getAllMedia();
-            media.forEach(mediaFile -> 
-                System.out.println(mediaFile.getFilename())
-            );
+            media = mediaService.dao().queryForAll();
+            int i = 0;
+            media.forEach(mediaFile -> {
+                
+                File file = new File(mediaFile.getFilename());
+                Image tempImage = new Image(file.toURI().toString());
+                PixelReader reader = tempImage.getPixelReader();
+                WritableImage newImage = null;
+                
+
+                if(tempImage.getHeight() == tempImage.getHeight()) {
+
+                } else if (tempImage.getHeight() < tempImage.getWidth()) {
+                    //höhe zum ausschneiden nehmen
+                    newImage = new WritableImage(reader, 0 , 0, (int) tempImage.getHeight(), (int) tempImage.getHeight());
+                } else {
+                    //breite zum ausschneiden nehmen
+                    newImage = new WritableImage(reader, 0 , 0, (int) tempImage.getWidth(), (int) tempImage.getWidth());
+                }
+
+                
+                ImageView imageView;
+                if (newImage != null) imageView = new ImageView(newImage);
+                else imageView = new ImageView(tempImage);
+
+                imageView.setFitHeight(200);
+                imageView.setPreserveRatio(true);
+                imageView.setSmooth(true);
+                imageView.setCache(true);
+
+
+                hb.getChildren().addAll(imageView);
+            });
+
+            scrollPane.setContent(hb);
+            return scrollPane;
 
         } catch (SQLException e) {
             // errorhandling
+            return null;
+
         }
-
-
-        scrollPane = new ScrollPane();
-        scrollPane.setHbarPolicy(ScrollBarPolicy.NEVER);
 
     }
 
