8ced21725eb254f7c5b89b89aa7ca854716aeca7
timm_a <timmandre23@gmail.com>
2021-06-14 16:06:46 +0200

    work on Album

--- app/src/main/java/events/ShowAlbumView.java
+++ app/src/main/java/events/ShowAlbumView.java
@@ -1,4 +1,16 @@
 package events;
 
+import entities.album.Album;
+import java.util.List;
+
 public class ShowAlbumView {
+    private List<Album> album;
+
+    public void ShowAlbums(List<Album> album) {
+        this.album = album;
+    }
+
+    public List<Album> getAlbum() {
+        return this.album;
+    }
 }
