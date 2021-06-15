346bb10415e40dfff4907331a0bf9e90019f9dca
timm_a <timmandre23@gmail.com>
2021-06-15 19:18:13 +0200

    improvements on albumview, switch from albumview to imagesview

--- app/src/main/java/views/AlbumViewController.java
+++ app/src/main/java/views/AlbumViewController.java
@@ -1,12 +1,36 @@
 package views;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
 
 public class AlbumViewController extends AbstractController implements Initializable {
+  @FXML
+  private FlowPane flowPane;
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     eventService.register(this);
+
+    var children = flowPane.getChildren();
+
+    try {
+      var albums = albumService.dao().queryForAll();
+      for (var album : albums) {
+        var albumBtn = new Button();
+        albumBtn.setText(album.getName());
+
+        children.add(albumBtn);
+      }
+    } catch (SQLException e) {
+      logger.log(Level.SEVERE, e.getMessage());
+    }
+
   }
 }
