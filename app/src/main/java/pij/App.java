1f2e6a5dd4b07416c945298643db639a6d269a4d
Andrej Benz <hello@benz.dev>
2021-06-23 14:27:18 +0200

    added fat jar

--- app/src/main/java/pij/App.java
+++ app/src/main/java/pij/App.java
@@ -1,49 +1,8 @@
 package pij;
 
-import entities.DatabaseConnectionService;
-import fr.brouillard.oss.cssfx.CSSFX;
-import java.io.File;
-import java.io.IOException;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import resources.Resource;
-import resources.ResourceService;
-import views.Loader;
-import views.SceneService;
-import views.View;
 
-public class App extends Application {
-  @Override
-  public void start(Stage stage) throws IOException {
-    CSSFX.start();
-    var i = Loader.getInjector();
-    var resourceService = i.getInstance(ResourceService.class);
-    var databaseService = i.getInstance(DatabaseConnectionService.class);
-    var sceneService = i.getInstance(SceneService.class);
-
-    resourceService.setContentFiles("mediafiles", "data.db");
-    databaseService.createSchema();
-
-    var pane = sceneService.load(View.MAINVIEW);
-    var scene = new Scene(pane);
-    sceneService.setRootScene(scene);
-
-    resourceService.setStageTitle(stage, Resource.CONFIG, View.MAINVIEW.toString());
-    stage.setScene(scene);
-    stage.setMaximized(true);
-    stage.centerOnScreen();
-
-    stage.show();
-  }
-
+public class App {
   public static void main(String[] args) {
-    var files = new File("mediafiles");
-
-    if (!files.exists()) {
-      files.mkdir();
-    }
-
-    launch();
+    Fx.main(args);
   }
 }
---
+++ app/src/main/java/pij/Fx.java
@@ -0,0 +1,49 @@
+package pij;
+
+import entities.DatabaseConnectionService;
+import fr.brouillard.oss.cssfx.CSSFX;
+import java.io.File;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import resources.Resource;
+import resources.ResourceService;
+import views.Loader;
+import views.SceneService;
+import views.View;
+
+public class Fx extends Application {
+  @Override
+  public void start(Stage stage) throws IOException {
+    CSSFX.start();
+    var i = Loader.getInjector();
+    var resourceService = i.getInstance(ResourceService.class);
+    var databaseService = i.getInstance(DatabaseConnectionService.class);
+    var sceneService = i.getInstance(SceneService.class);
+
+    resourceService.setContentFiles("mediafiles", "data.db");
+    databaseService.createSchema();
+
+    var pane = sceneService.load(View.MAINVIEW);
+    var scene = new Scene(pane);
+    sceneService.setRootScene(scene);
+
+    resourceService.setStageTitle(stage, Resource.CONFIG, View.MAINVIEW.toString());
+    stage.setScene(scene);
+    stage.setMaximized(true);
+    stage.centerOnScreen();
+
+    stage.show();
+  }
+
+  public static void main(String[] args) {
+    var files = new File("mediafiles");
+
+    if (!files.exists()) {
+      files.mkdir();
+    }
+
+    launch();
+  }
+}
